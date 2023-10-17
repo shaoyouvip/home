e1c58ccb6045b3217d8de8f8794c6ccd13625714
JokerYue <Joker_Yue@qq.com>
2023-10-17 18:38:01 +0800

    全部修改到joker2yue.cn

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,13 +43,14 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Brain } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
 import siteLinks from "@/assets/siteLinks.json";
 import "swiper/scss";
 import "swiper/scss/pagination";
+import { computed, onMounted } from "vue";
 
 const store = mainStore();
 
@@ -72,6 +73,7 @@ const siteIcon = {
   Book,
   Fire,
   LaptopCode,
+  Brain,
 };
 
 // 链接跳转
