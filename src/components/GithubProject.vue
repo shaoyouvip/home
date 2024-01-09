45b489374721ab25ae1cf5b3b3d54d5fd52608c3
星河 <weizixuan110@foxmail.com>
2024-01-10 04:32:17 +0800

    Update GithubProject.vue

--- src/components/GithubProject.vue
+++ src/components/GithubProject.vue
@@ -1,1 +1,42 @@
-
+<template>
+  <div class="github-project">
+    <div class="title">
+      <github-one theme="two-tone" size="24" :fill="['#efefef', '#00000020']" />
+      <span>Github</span>
+    </div>
+    <Swiper
+      :modules="[Pagination, Mousewheel]"
+      :slides-per-view="1"
+      :space-between="40"
+      :pagination="{
+        el: '.swiper-pagination',
+        clickable: true,
+        bulletElement: 'div',
+      }"
+      :mousewheel="true"
+    >
+      <SwiperSlide v-for="list in projectList" :key="list">
+        <el-row class="all-project" :gutter="20">
+          <el-col v-for="(item, index) in list" :span="12" :key="index">
+            <div class="project cards" @click="toGithub(item)">
+              <div class="name">
+                <bookmark theme="outline" size="22" fill="#efefef" />
+                <div class="name-text">
+                  <span class="author">{{ item.author }}</span>
+                  <span>{{ item.name }}</span>
+                </div>
+              </div>
+              <span class="desc">{{ item.desc }}</span>
+            </div>
+          </el-col>
+        </el-row>
+      </SwiperSlide>
+      <div class="swiper-pagination" />
+    </Swiper>
+  </div>
+</template>
+<script setup>
+import { GithubOne, Bookmark } from "@icon-park/vue-next";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination, Mousewheel } from "swiper";
+  </script>
