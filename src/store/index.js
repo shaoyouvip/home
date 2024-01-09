b00e602273b2e79f244b00820ec23c7565b6716a
星河 <weizixuan110@foxmail.com>
2024-01-10 04:00:19 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -12,9 +12,9 @@ export const mainStore = defineStore("main", {
       musicVolume: 0, // 音乐音量;
       musicOpenState: true, // 音乐面板开启状态
       backgroundShow: false, // 壁纸展示状态
-      boxOpenState: true, // 盒子开启状态
-      mobileOpenState: true, // 移动端开启状态
-      mobileFuncState: true, // 移动端功能区开启状态
+      boxOpenState: false, // 盒子开启状态
+      mobileOpenState: false, // 移动端开启状态
+      mobileFuncState: false, // 移动端功能区开启状态
       setOpenState: false, // 设置页面开启状态
       playerState: false, // 当前播放状态
       playerTitle: null, // 当前播放歌曲名
