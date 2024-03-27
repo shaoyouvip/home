467118131221cb3b6ef33bb77f21ef9b32539942
Ji Shuqin <118960740+Gumengyo@users.noreply.github.com>
2024-03-27 16:01:51 +0800

    引入Robot图标

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -42,7 +42,7 @@
 
 <script setup>
 import { Icon } from "@vicons/utils";
-// 可前往 https://www.xicons.org 自行挑选并在此处引入
+// 可前往 https://www.xicons.org 自行挑选并在此处引入 
 import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Robot } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
@@ -70,6 +70,7 @@ const siteIcon = {
   Book,
   Fire,
   LaptopCode,
+  Robot
 };
 
 // 链接跳转
