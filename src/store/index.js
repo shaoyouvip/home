974bf80308af95bf7ec51a79fda92992fb5fec3e
shuaishuai <shuaidaoya@gmail.com>
2024-04-24 14:20:40 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ export const mainStore = defineStore("main", {
       imgLoadStatus: false, // 壁纸加载状态
       innerWidth: null, // 当前窗口宽度
       coverType: "0", // 壁纸种类
-      siteStartShow: false, // 建站日期显示
+      siteStartShow: true, // 建站日期显示
       musicClick: false, // 音乐链接是否跳转
       musicIsOk: false, // 音乐是否加载完成
       musicVolume: 0, // 音乐音量;
@@ -15,7 +15,7 @@ export const mainStore = defineStore("main", {
       boxOpenState: false, // 盒子开启状态
       mobileOpenState: false, // 移动端开启状态
       mobileFuncState: false, // 移动端功能区开启状态
-      setOpenState: false, // 设置页面开启状态
+      setOpenState: true, // 设置页面开启状态
       playerState: false, // 当前播放状态
       playerTitle: null, // 当前播放歌曲名
       playerArtist: null, // 当前播放歌手名
