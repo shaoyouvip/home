7d34951365d2001df74747a7c3501dc09e0633d0
shuaishuai <shuaidaoya@gmail.com>
2024-04-24 14:38:35 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -15,11 +15,11 @@ export const mainStore = defineStore("main", {
       boxOpenState: false, // 盒子开启状态
       mobileOpenState: false, // 移动端开启状态
       mobileFuncState: false, // 移动端功能区开启状态
-      setOpenState: true, // 设置页面开启状态
+      setOpenState: false, // 设置页面开启状态
       playerState: false, // 当前播放状态
       playerTitle: null, // 当前播放歌曲名
       playerArtist: null, // 当前播放歌手名
-      playerLrc: "歌词加载中", // 当前播放歌词
+      playerLrc: "帅帅提醒您-歌词加载中", // 当前播放歌词
       playerLrcShow: true, // 是否显示底栏歌词
       footerBlur: true, // 底栏模糊
       playerAutoplay: false, // 是否自动播放
