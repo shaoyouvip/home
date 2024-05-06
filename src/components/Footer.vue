e7d75d8659461545521dd9f74ff6cbf7f7e36010
lhkuan <69495276+lhkuan@users.noreply.github.com>
2024-05-06 16:33:51 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,11 +13,7 @@
         </span>
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">
-          &amp;&nbsp;Made&nbsp;by
-          <a :href="config.github" target="_blank">
-            {{ config.author }}
-          </a>
-          
+          &amp;&nbsp;powered&nbsp;by&nbsp;lidu
         </span>
         <!-- 站点备案 -->
         <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
