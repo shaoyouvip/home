75256aeee38318a499b48daaf1f33ec411bd8223
241zzzz <241815721@qq.com>
2024-06-01 10:13:38 +0800

    Update Message.vue

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -3,7 +3,7 @@
   <div class="message">
     <!-- Logo -->
     <div class="logo">
-      <img class="logo-img" :src="siteLogo" alt="logo" />
+      <img class="logo-img" :src="/public/images/icon/mao.jpg" alt="logo" />
       <div :class="{ name: true, 'text-hidden': true, long: siteUrl[0].length >= 6 }">
         <span class="bg">{{ siteUrl[0] }}</span>
         <span class="sm">.{{ siteUrl[1] }}</span>
