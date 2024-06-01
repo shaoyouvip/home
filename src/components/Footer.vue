c0eafb85d72038b83747b5275ec57ac2623ec11f
241zzzz <241815721@qq.com>
2024-06-01 09:49:40 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,7 @@
             -
           </span>
           {{ fullYear }}
-          <a :href="siteUrl">{{ siteAnthor }}</a>
+          <a :href="https://www.march317.top/">{{ March }}</a>
         </span>
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">
