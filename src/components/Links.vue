a1c89839279d5e52d0c3f8e7e2fe73d3b796d747
周润发 <shaoyouvip@gmail.com>
2025-01-23 00:28:12 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Music } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -69,6 +69,7 @@ const siteIcon = {
   Compass,
   Book,
   Fire,
+  Music,
   LaptopCode,
 };
 
