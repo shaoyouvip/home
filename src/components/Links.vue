39853194f11f7b08c1f2ecc1c5cf97044b11f7b4
周润发 <shaoyouvip@gmail.com>
2025-03-23 23:25:55 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Music, InternetExplorer, FileImage, ExchangeAlt, Envelope, Bullseye, Magic, CodeBranch, Icons, Newspaper, Video,  Markdown, Soundcloud, Cloud, Tools, Images } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Music, InternetExplorer, FileImage, ExchangeAlt, Envelope, Bullseye, Magic, CodeBranch, Icons, Newspaper, Video,  Markdown, Soundcloud, Tools, Images } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -77,7 +77,6 @@ const siteIcon = {
   Markdown,
   Link,
   Tools,
-  Cloud,
   Soundcloud,
   Video,
   Newspaper,
